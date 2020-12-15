82d473bfaa844c1bd48cf11a139db82e4dd3b7a3
Rohith04MVK <007rohitjj04@gmail.com>
2020-12-15 20:12:11 +0530

    Added capitalizer and some linting

---
+++ src/capitalizer.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string capitalizer(string text)
+{
+
+    for (int i = 0; i < text.length(); i++)
+    {
+        if (i == 0)
+        {
+            text[i] = toupper(text[i]);
+        }
+
+        else if (text[i - 1] == ' ')
+        {
+            text[i] = toupper(text[i]);
+        }
+       
+    }
+     return text;
+}
+
+int main()
+{
+    cout << "\n"
+         << capitalizer("capitals are boring");
+    return 0;
+}
