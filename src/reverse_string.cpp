139d32d5cefbdf282c8c8d0f9d7377d7456e049c
Rohith04MVK <007rohitjj04@gmail.com>
2020-12-16 16:21:37 +0530

    Added string reverser.

---
+++ src/reverse_string.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    string str;
+    cout << "Enter a word: ";
+    cin >> str;
+
+    reverse(str.begin(), str.end());
+    cout << str;
+    return 0;
+}
