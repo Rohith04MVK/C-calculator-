2db0e79436d83544244e0b5e935e89087e7af4c6
Rohith04MVK <007rohitjj04@gmail.com>
2020-12-17 19:49:58 +0530

    Added dice simulator

---
+++ src/DiceSim.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int Dice(int range1, int range2, int count){
+
+    int i;
+
+    for(i = 0; i < count; i++)
+    {
+        int num = rand() % (range1 - range2 + 1) + range1;
+        cout << num;
+
+    }
+}
+
+int main(){
+
+    int range1 = 1, range2 = 10, count = 2;
+
+    Dice(range1, range2, count);
+}
