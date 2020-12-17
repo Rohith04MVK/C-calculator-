dbd413d51c321f13f61f9cc98cbb74243a9167a2
Rohith04MVK <007rohitjj04@gmail.com>
2020-12-17 20:01:01 +0530

    Added some linting.

--- src/DiceSim.cpp
+++ src/DiceSim.cpp
@@ -4,20 +4,20 @@
 
 using namespace std;
 
-void Dice(int range1, int range2, int count){
+void Dice(int range1, int range2, int count)
+{
 
     int i;
 
-    for(i = 0; i < count; i++)
+    for (i = 0; i < count; i++)
     {
         int num = rand() % (range2 - range1 + 1) + range1;
         cout << num << " ";
-        
     }
-
 }
 
-int main(){
+int main()
+{
 
     int range1 = 1, range2 = 10, count = 5;
 
