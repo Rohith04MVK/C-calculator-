692ca975b94262019147c9582a4759754fc14d9a
Rohith04MVK <007rohitjj04@gmail.com>
2020-12-18 08:50:15 +0530

    Adding my toupper

--- src/my_toupper.cpp
+++ src/my_toupper.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 char uppercase(char bla)
 {
     return bla -('a'-'A');
+}
+
+string capitalizer(string text)
+{
+
+    for (int i = 0; i < text.length(); i++)
+    {
+        if (i == 0)
+        {
+            text[i] = toupper(text[i]);
+        }
+
+        else if (text[i - 1] == ' ')
+        {
+            text[i] = toupper(text[i]);
+        }
+    }
+    return text;
 }
