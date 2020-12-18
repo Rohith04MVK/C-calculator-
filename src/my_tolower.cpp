1d7744fea7f2748aad25e34b81474a055b69fec0
Rohith04MVK <007rohitjj04@gmail.com>
2020-12-18 09:06:47 +0530

    Highly buggy to lower program.

--- src/my_tolower.cpp
+++ src/my_tolower.cpp
@@ -6,7 +6,19 @@ char lowercase(char alpha){
     return alpha - ('A' - 'a');
 }
 
-int main(){
-    cout << lowercase('H');
+string lower(string text)
+{
+
+    for (int i = 0; i < text.length(); i++){
+        text[i] = lowercase(text[i]);
+
+    }
+    return text;
+}
+
+int main()
+{
+    cout << "\n"
+         << lower("Heallo my friend");
     return 0;
 }
