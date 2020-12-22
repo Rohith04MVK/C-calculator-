70a42afdaa548076ed3b3cf93cbdeb17a0b7d20e
Rohith04MVK <007rohitjj04@gmail.com>
2020-12-22 08:50:08 +0530

    Fixed Leap year finder

--- src/Projects/LeapYear.cpp
+++ src/Projects/LeapYear.cpp
@@ -10,7 +10,7 @@ int main()
     cout << "Enter a year in YYYY format: ";
     cin >> year;
 
-    if (year % 4 == 0)
+    if (year % 4 == 0 and year % 100 != 0 or year % 400 == 0)
     {
         cout << year << " is a leap year";
     }
@@ -18,5 +18,8 @@ int main()
     {
         cout << year << " is not a leap year";
     }
+    
+    
+
     return 0;
 }
