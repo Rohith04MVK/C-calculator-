8f203b47500555951fcc010d275f597b8c04d4ad
Rohith04MVK <007rohitjj04@gmail.com>
2020-12-29 12:19:40 +0530

    Added some liniting and cleared white sapces.

--- src/Projects/factorial.cpp
+++ src/Projects/factorial.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
     int num, factorial = 1;
-    cout << " Enter Number To Find Its Factorial:  ";
+    cout << " Enter Number To Find Its Factorial: ";
     cin >> num;
     for (int a = 1; a <= num; a++)
     {
