8e7a29b9c4355929eb9a7aee60cfbfb761c2a9ed
Rohith04MVK <007rohitjj04@gmail.com>
2021-01-14 19:24:40 +0530

    Added vector array code

--- src/Fundamentals/vector_array.cpp
+++ src/Fundamentals/vector_array.cpp
@@ -5,5 +5,17 @@ using namespace std;
 
 int main()
 {
-    vector<int> g1;
+    vector<int> my_array;
+
+    for (int i = 0; i < 10; i++)
+    {
+        my_array.push_back(i);
+    }
+
+    for (auto i = my_array.begin(); i != my_array.end(); ++i)
+    {
+        cout << *i << " ";
+    }
+
+    return 0;
 }
